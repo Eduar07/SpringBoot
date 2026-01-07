ab197310fbb14acdb20d85bf450c1f2a68116880
Eduar guerrero <edhuarguerrero@gmail.com>
2026-01-07 13:03:38 -0500

    Primera clase, ajustando

--- src/main/java/com/eduar/firsteps/infrastructure/controller/HelloWorldController.java
+++ src/main/java/com/eduar/firsteps/infrastructure/controller/HelloWorldController.java
@@ -2,8 +2,10 @@ package com.eduar.firsteps.infrastructure.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.HashMap;
+import java.util.Map;
 
 /*Autowired, Realiza la inyeccion de dependicias automatica (sroing busca la instancia necesaria y la asigna) */
 /*Service, marcan clases como logica de negocio o acceso o datos, repctivamente, para que los spring las gestione */
@@ -13,8 +15,16 @@ comportamiento especial, define una clase como un contrlador web que retorna dat
 public class HelloWorldController {
 
     @GetMapping("/greetings")  /* MApea solicitures HTTP GET a un metodo especifivo */
-    public String holaMundo(){
-        return "Hola Camper";
+    public String holaMundo(@RequestParam(name = "name", defaultValue = "Ponscia") String nombre){
+        return "Hola " + nombre + " Camper";
+    }
+
+    @GetMapping("/json")
+    public Map<String, String> getMethodName(@RequestParam String name){
+        Map<String, String> reponse = new HashMap<>();
+        reponse.put("greeting" , "Hellou everinain");
+        reponse.put("user", name);
+        return reponse;
     }
 }
 /*inyecta valos de configuracion  defnidios externamiente, desde aplicacion properties
